532a72dd1771c99602b6cff7214e53407b84aeaf
AbdoMady2002 <abdomady197@gmail.com>
2023-03-03 17:33:38 +0200

    add custom app bar to widget folder

---
+++ lib/view/widget/custom_App_Bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class customAppBar extends StatelessWidget {
+  const customAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row();
+  }
+}
--- lib/view/widget/note_view_body.dart
+++ lib/view/widget/note_view_body.dart
@@ -1,11 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'custom_App_Bar.dart';
 
 class noteviewbody extends StatelessWidget {
   const noteviewbody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Column(
+      children: [
+        customAppBar()
+
+      ],
+    );
   }
 }
