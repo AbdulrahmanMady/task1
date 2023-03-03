340fa9b4049b0d5719f5e7903633b6a1d6565c85
AbdoMady2002 <abdomady197@gmail.com>
2023-03-03 15:57:37 +0200

    change background To Dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+//import 'package:tsk1/view/note_view.dart';
 
 void main() {
   runApp(const notesapp());
 }
+// ignore: camel_case_types
 class notesapp extends StatelessWidget {
   const notesapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+     // home: const notesView(),
+
+    );
   }
 }
