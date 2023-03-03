c1d7e343d66dfd1f992d50e37309784597e88d34
AbdoMady2002 <abdomady197@gmail.com>
2023-03-03 21:21:52 +0200

    create BoxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:tsk1/view/notes_view.dart';
 
 void main() {
-  runApp(const notesapp());
+  runApp(const Notesapp());
 }
 // ignore: camel_case_types
-class notesapp extends StatelessWidget {
-  const notesapp({super.key});
+class Notesapp extends StatelessWidget {
+  const Notesapp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
-      home: const notesView(),
+      theme: ThemeData(brightness: Brightness.dark ),
+      home: const NotesView(),
 
     );
   }
