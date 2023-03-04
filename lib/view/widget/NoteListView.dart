fc918b88c62faf6d9f20a1f3d1e67dee1303a94e
AbdoMady2002 <abdomady197@gmail.com>
2023-03-04 23:18:56 +0200

    make padding betwean notes

--- lib/view/widget/NoteListView.dart
+++ lib/view/widget/NoteListView.dart
@@ -8,11 +8,15 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context,index){
+    return Padding(padding: const EdgeInsets.symmetric(vertical: 16)
+    ,child: ListView.builder(padding: EdgeInsets.zero,itemBuilder: (context,index)
+    {
       return const Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
+        padding: const EdgeInsets.symmetric(vertical: 6),
         child: const NoteItem(),
-      );
-    });
+        );
+    },),
+    );
+     
+    }
   }
-}
