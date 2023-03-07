29a8df2581a85a61385b2b9d0a1f3491bee6b05d
AbdoMady2002 <abdomady197@gmail.com>
2023-03-08 00:04:58 +0200

    Finish Add Note Bottom Sheet

--- lib/view/widget/AddNoteBottom.dart
+++ lib/view/widget/AddNoteBottom.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tsk1/constants.dart';
 
 import 'custom.text.filed.dart';
 
@@ -10,11 +12,54 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16)
-      ,child: Column(
-        children: [
-          SizedBox(height: 32,)
-          ,CustomTextfiled(),
+      ,child:SingleChildScrollView(
+
+      child: Column(
+        children: const[
+          SizedBox(
+            height: 32,
+            )
+          , CustomTextfiled(
+            hint: 'Title',
+            ),
+           SizedBox(
+            height: 16,
+            )
+          , CustomTextfiled(
+            hint: 'Content',
+            maxlines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomBotton(),
+            SizedBox(
+              height: 16,
+            )
+         
         ],
-      ),);
+      ),
+    );
+  }
+}
+class CustomBotton extends StatelessWidget {
+  const CustomBotton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: kPrimrayColor
+        ,borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+          child: Text('Save' ,style: TextStyle(color:Colors.black,
+          fontWeight: FontWeight.normal,
+          fontSize: 20
+          ),
+        ),
+        )
+    );
   }
 }
--- lib/view/widget/custom.text.filed.dart
+++ lib/view/widget/custom.text.filed.dart
@@ -6,14 +6,16 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:tsk1/constants.dart';
 
 class CustomTextfiled extends StatelessWidget {
-  const CustomTextfiled({super.key});
-
+  const CustomTextfiled({super.key ,required this.hint , this.maxlines=1});
+  final String hint;
+  final int maxlines;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimrayColor,
+      maxLines: maxlines,
       decoration: InputDecoration(
-        hintText: 'Title',
+        hintText: hint,
       border: BuildBorder(),
       enabledBorder: BuildBorder(),
       focusedBorder: BuildBorder(kPrimrayColor
