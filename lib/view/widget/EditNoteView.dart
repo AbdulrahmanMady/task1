93aeaa638d2f61631b77b2edf9c6aacbb9ed5d1b
AbdoMady2002 <abdomady197@gmail.com>
2023-03-08 18:18:42 +0200

    EditNoteView UI Done

--- lib/view/widget/EditNoteView.dart
+++ lib/view/widget/EditNoteView.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:tsk1/view/widget/EditViewBody.dart';
 
 class EditNoteView extends StatelessWidget {
   const EditNoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Scaffold(
+      body: EditNoteViewBody(),
+     
+      
+    );
    
   }
 }
