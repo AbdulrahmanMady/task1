8d24e2c64267633aab7dca41d740d42ec577c699
AbdoMady2002 <abdomady197@gmail.com>
2023-03-08 18:22:02 +0200

    EditNoteView ui done

--- lib/view/widget/EditViewBody.dart
+++ lib/view/widget/EditViewBody.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:tsk1/view/widget/Custom_App_Bar.dart';
+import 'package:tsk1/view/widget/custom.text.filed.dart';
 
-class EditViewBody extends StatelessWidget {
-  const EditViewBody({super.key});
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,15 @@ class EditViewBody extends StatelessWidget {
         ),
         CustomAppBar(title: 'Edit Note'
         , icon: Icons.check
-        )
+        ),
+         SizedBox(
+          height: 50,
+        ),
+        CustomTextfiled(hint: 'Title',),
+        SizedBox(
+          height: 16,
+        ),
+        CustomTextfiled(hint: 'Content' ,maxlines: 5,)
       ]),
     );
   }
